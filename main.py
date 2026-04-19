ddc2de48c4df935b63996de742747d593fbe41da
Yaba Louo Jean de DIEU <gauisyaba@gmail.com>
2026-04-19 16:55:52 +0000

    first base script

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from uuid import UUID
-from enum import Enum
-from datetime import datetime
 
 app = FastAPI()
 
-class TaskStatus(str, Enum):
-    active = "active"
-    description = "description"
 
-
-class Task(BaseModel):
-    id: UUID
-    content: str
-    status: TaskStatus
-    created: datetime
-    updated: datetime
-
-
-@app.get('/{task}')
-async def main(task: Task):
+@app.get('/')
+async def main():
     return {"message": "Hello world"}
